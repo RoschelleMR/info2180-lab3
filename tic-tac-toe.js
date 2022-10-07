700b59e742ec0ddf33f7e16a313f14584db7f104
Roschelle Matthews-Rhoden <roschelle2k6@gmail.com>
2022-10-07 02:04:43 -0500

    Added layout, tic-tac-toe.js

---
+++ tic-tac-toe.js
@@ -0,0 +1,12 @@
+
+document.addEventListener('DOMContentLoaded', function(){
+    // layout
+        var boxes = document.querySelectorAll("#board div");
+    
+        for (var i = 0; i < boxes.length; i++) {
+            boxes[i].classList.add('square');
+        }
+    // end of layout
+    
+    
+})
