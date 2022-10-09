700882f971127078197ea292d2cb73321d6d7741
Roschelle Matthews-Rhoden <roschelle2k6@gmail.com>
2022-10-09 16:00:56 -0500

    Completed Exercise 2

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,12 +1,60 @@
 
 document.addEventListener('DOMContentLoaded', function(){
-    // layout
-        var boxes = document.querySelectorAll("#board div");
-    
-        for (var i = 0; i < boxes.length; i++) {
-            boxes[i].classList.add('square');
+
+    const win_combins = [ //all possible positions to win
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,4,8],
+        [2,5,6],
+        [0,3,6],
+        [2,5,8],
+        [1,4,7]
+    ]
+
+    let player = "X"
+    let placements = [];
+
+    const board = document.querySelector("#board");
+    const boxes = document.querySelectorAll("#board div");
+    let status = document.querySelector("#status");
+
+    function switch_player(){ //switches players
+        if (player=="X"){
+            player = "O"
         }
-    // end of layout
-    
+        else{
+            player = "X"
+        }
+    }
+
+
+    boxes.forEach(function(box, index){
+        box.classList.add('square'); // layout
+        box.addEventListener("click", function(e){
+
+            if (player == "X"){
+                box.classList.add("X")
+            }
+            else{
+                box.classList.add("O");
+            }
+
+            box.innerHTML = player;
+            
+
+            placements.push(player);
+            console.log(placements);
+
+            switch_player()
+            status.innerHTML= `${player}'s turn`
+
+
+            
+            
+        }, {once: true}) //once ensures that the player can only click 1 square at a time
+    })
+
+        
     
 })
