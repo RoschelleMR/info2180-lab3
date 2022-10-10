30d5d89d10ba45fdf53834a077691fc341cd6be8
Roschelle Matthews-Rhoden <roschelle2k6@gmail.com>
2022-10-09 23:19:43 -0500

    Completed All Exercises by Editing tic-tac-toe.js

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,23 +1,28 @@
 
 document.addEventListener('DOMContentLoaded', function(){
 
-    const win_combins = [ //all possible positions to win
-        [0,1,2],
-        [3,4,5],
-        [6,7,8],
-        [0,4,8],
-        [2,4,6],
-        [0,3,6],
-        [2,5,8],
-        [1,4,7]
-    ]
+    // const win_combins = [ //all possible positions to win
+    //     [0,1,2],
+    //     [3,4,5],
+    //     [6,7,8],
+    //     [0,4,8],
+    //     [2,4,6],
+    //     [0,3,6],
+    //     [2,5,8],
+    //     [1,4,7]
+    // ]
+
+    let gameState = true; //game running
 
     let player = "X" //default player is X
     let placements = []; //Empty array to store selctions
 
     const board = document.querySelector("#board");
     const boxes = board.querySelectorAll("div");
-    let status = document.querySelector("#status");
+    const status = document.querySelector("#status");
+
+    let controls = document.querySelector(".controls" )
+    const newGame = controls.querySelector('.btn');
 
 
 
@@ -34,6 +39,8 @@ document.addEventListener('DOMContentLoaded', function(){
     boxes.forEach(function(box, index){
         box.classList.add('square'); // layout
 
+
+
         box.addEventListener("mouseover", () => { //hover in event
             box.style.cursor = "pointer";
             box.classList.add("hover");
@@ -43,32 +50,41 @@ document.addEventListener('DOMContentLoaded', function(){
             box.classList.remove("hover");
         });
 
+
+
+        
         box.addEventListener("click", function(e){ //click event
 
-            if (player == "X"){
-                box.classList.add("X")
+            if (gameState == true){
+
+                if (player == "X"){
+                    box.classList.add("X")
+                }
+                else{
+                    box.classList.add("O");
+                }
+
+                box.innerHTML = player;
+                
+
+                placements[index] = player //adding the selection to the same box number in the array 
+
+
+                if (winner(index,player) == true){ 
+                    gameState = false;
+                    
+                    status.classList.add("you-won");
+                    status.innerHTML = `Congratulations! ${player} is the Winner!`;
+
+                };
+                
+                switch_player()
+            
             }
-            else{
-                box.classList.add("O");
-            }
-
-            box.innerHTML = player;
             
-
-            placements[index] = player //adding the selection to the same box number in the array 
-
-
-            if (winner(index,player) == true){ 
-                status.classList.add("you-won");
-                status.innerHTML = `Congratulations! ${player} is the Winner!`;
-
-                //end game here
-            };
-
-            switch_player()
-            
-            
-        }, {once: true}) //once ensures that the player can only click 1 square at a time
+        })
+        
+        
     })
 
 
@@ -147,6 +163,27 @@ document.addEventListener('DOMContentLoaded', function(){
 
         return false; 
      };
+
+
+     newGame.addEventListener("click", () => { //new game button
+        
+        boxes.forEach(box => {
+            box.classList.remove("X")
+            box.classList.remove("O")
+            box.innerHTML="";
+
+        
+        });
+
+        placements = [] //clear array
+
+        status.classList.remove("you-won")
+        status.innerHTML = "Move your mouse over a square and click to play an X or an O."
+
+        gameState = true; //restart game
+        player = "X";
+
+     })
         
     
 })
